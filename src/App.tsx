0c252a7d1a2469edccbb153f2ead1d26cd6074bd
dtromans <29309408+dtromans@users.noreply.github.com>
2022-06-13 00:00:44 -0400

    implemented api and delete button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
+import ReminderService from './services/reminder';
+
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+  
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await ReminderService.getReminders();
+    setReminders(reminders);
+  }
+
+  const removeReminder = (id: number) => {
+    setReminders(reminders.filter(reminder => reminder.id !== id));
+  }
+
   return (
     <div className="App">
-      <button className="btn btn-primary">Click Me</button>
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
